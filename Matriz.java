bbd0def6f691404c9be2f2208ee94f52b447564d
alvarowkz <alvarowkz@gmail.com>
2025-04-19 00:29:00 +0200

    Segundo reto.

--- Matriz.java
+++ Matriz.java
@@ -2,19 +2,14 @@ package hause.retos.difMatriz;
 
 public class Matriz {
 
-	public static void main(String[] args) {
-
-		int sumaPrincipal = 0, sumaSecundaria = 0, dif;
-
-		int[][] matriz = { { 11, 2, 4 }, { 4, 5, 6 }, { 10, 8, -12 } };
-
-		// mostramos raíz
-		for (int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j < matriz.length; j++) {
-				System.out.print(matriz[i][j] + "\t");
-			}
-			System.out.println();
-		}
+	/**
+	 * Función que devuelve una resta de ambas diagonales de una raíz cuadrada
+	 * 
+	 * @param matriz
+	 * @return la diferencia de ambas sumas.
+	 */
+	public static int restaDiagonales(int[][] matriz) {
+		int sumaPrincipal = 0, sumaSecundaria = 0;
 
 		// suma diagonal primaria
 		for (int i = 0; i < matriz.length; i++) {
@@ -26,11 +21,24 @@ public class Matriz {
 		for (int i = matriz.length - 1; i >= 0; i--) {
 			sumaSecundaria += matriz[i][j];
 			j++;
-		}	
+		}
 
-		dif = Math.abs(sumaPrincipal - sumaSecundaria);
+		return Math.abs(sumaPrincipal - sumaSecundaria);
+	}
 
-		System.out.println("\nDiferencia absoluta entre diagonales: " + dif);
+	public static void main(String[] args) {
+
+		int[][] matriz = { { 11, 2, 4 }, { 4, 5, 6 }, { 10, 8, -12 } };
+
+		// mostramos raíz
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				System.out.print(matriz[i][j] + "\t");
+			}
+			System.out.println();
+		}
+
+		System.out.println("\nDiferencia absoluta entre diagonales: " + restaDiagonales(matriz));
 
 	}
 
